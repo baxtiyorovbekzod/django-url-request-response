0872768fa33d2302964c77a15849aad14ff613a5
djumanov <djumanovdev@gmail.com>
2026-01-06 10:18:54 +0500

    calculators view

--- users/views.py
+++ users/views.py
@@ -23,6 +23,42 @@ def orders_view(request: HttpRequest) -> HttpResponse:
 def calculators_view(request: HttpRequest) -> HttpResponse:
 
     # ?num1=3&num2=7&op=+
-    
-    return HttpResponse(f'result: ') # 'result: 3+7=10
+    query_params = request.GET
 
+    # validation
+    if query_params.get('num1') is None:
+        return HttpResponse('num1 is required.')
+    if query_params.get('num2') is None:
+        return HttpResponse('num2 is required.')
+    if query_params.get('op') not in ['add', 'mul', 'sub', 'div']:
+        return HttpResponse("op should be ('add', 'mul', 'sub', 'div').")
+
+    # calculate
+    num1 = int(query_params.get('num1'))
+    num2 = int(query_params.get('num2'))
+    op = query_params.get('op')
+    
+    # if op == 'add':
+    #     result = f'{num1}+{num2}={num1 + num2}'
+    # elif op == 'sub':
+    #     result = f'{num1}-{num2}={num1 - num2}'
+    # elif op == 'mul':
+    #     result = f'{num1}*{num2}={num1 * num2}'
+    # elif op == 'div':
+    #     result = f'{num1}/{num2}={num1 / num2}'
+    # else:
+    #     result = 'operator not found.'
+
+    match op:
+        case 'add':
+            result = f'{num1}+{num2}={num1 + num2}'
+        case 'sub':
+            result = f'{num1}-{num2}={num1 - num2}'
+        case 'mul':
+            result = f'{num1}*{num2}={num1 * num2}'
+        case 'div':
+            result = f'{num1}/{num2}={round(num1 / num2, 2)}'
+        case _:
+            result = 'operator not found.'
+    
+    return HttpResponse(f'result: {result}') # 'result: 3+7=10
